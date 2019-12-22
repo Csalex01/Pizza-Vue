22570a30b2cfcc5d7e862b25283e8c33cc831320
Csillag Alex <csillagalex2001@gmail.com>
2019-12-22 22:52:23 +0200

    Added more feautres to the Card component

--- Pizza-Vue/src/components/index/Card.vue
+++ Pizza-Vue/src/components/index/Card.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="row">
+  <!-- <div class="row">
     <div class="col s12 m4">
-      <div class="card blue-grey darken-1">
-        <div class="card-content white-text">
+      <div class="card small darken-1">
+        <div class="card-content dark-text">
           <span class="card-title">Card Title</span>
           <p>
             I am a very simple card. I am good at containing small bits of information.
@@ -12,6 +12,34 @@
         <div class="card-action">
           <a href="#">This is a link</a>
           <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>-->
+
+  <div class="row">
+    <div class="col s12 m4">
+      <div class="card">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img src="https://materializecss.com/images/office.jpg" alt class="activator" />
+        </div>
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">
+            Card title
+            <i class="material-icons right">more_vert</i>
+          </span>
+          <p>Basic description about the product</p>
+        </div>
+        <div class="card-action">
+          <a href="#">Order</a>
+          <a href="#">Details</a>
+        </div>
+        <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4">
+            Card title
+            <i class="material-icons right">close</i>
+          </span>
+          <p>Here is some more information about this product that is only revealed once clicked on.</p>
         </div>
       </div>
     </div>
@@ -33,6 +61,4 @@ export default {
 </script>
 
 <style>
-.card {
-}
 </style>
